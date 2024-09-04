018d12728b2c049d210512134ffa336e3646cd23
cortez24rus <89983388+cortez24rus@users.noreply.github.com>
2024-09-05 02:46:04 +0700

    Update 3x-ui-server.sh

--- 3x-ui-server.sh
+++ 3x-ui-server.sh
@@ -198,7 +198,21 @@ dns_encryption() {
 
 dns_adguard_home() {
 	echo -e "${blue}Настройка Adguard-home (DoH)${clear}"
-	echo "DNS=9.9.9.9"
+	cat > /etc/systemd/resolved.conf <<EOF
+[Resolve]
+# Some examples of DNS servers which may be used for DNS= and FallbackDNS=:
+# Cloudflare: 1.1.1.1#cloudflare-dns.com 1.0.0.1#cloudflare-dns.com 2606:4700:4700::1111#cloudflare-dns.com 2606:4700:4700::1001#cloudflare-dns.com
+# Google:     8.8.8.8#dns.google 8.8.4.4#dns.google 2001:4860:4860::8888#dns.google 2001:4860:4860::8844#dns.google
+# Quad9:      9.9.9.9#dns.quad9.net 149.112.112.112#dns.quad9.net 2620:fe::fe#dns.quad9.net 2620:fe::9#dns.quad9.net
+DNS=9.9.9.9
+#FallbackDNS=
+Domains=~.
+DNSSEC=yes
+DNSOverTLS=yes
+EOF
+
+	systemctl restart systemd-resolved.service
+	
 	wget https://static.adguard.com/adguardhome/release/AdGuardHome_linux_amd64.tar.gz
 	tar xvf AdGuardHome_linux_amd64.tar.gz
 	AdGuardHome/AdGuardHome -s install
@@ -411,7 +425,6 @@ EOF
 dns_systemd_resolved() {
 	echo -e "${blue}Настройка systemd-resolved (DoT)${clear}"
 	echo "DNS=9.9.9.9"
-	AdGuardHome/AdGuardHome -s uninstall
 	
 cat > /etc/systemd/resolved.conf <<EOF
 [Resolve]
