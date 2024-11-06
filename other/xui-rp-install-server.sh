d73b631c5e5dc679c97913eeef90ace61c1cb8a9
cortez24rus <89983388+cortez24rus@users.noreply.github.com>
2024-11-06 23:01:18 +0700

    Update xui-rp-install-server.sh

--- other/xui-rp-install-server.sh
+++ other/xui-rp-install-server.sh
@@ -580,7 +580,6 @@ worker_processes                  auto;
 worker_rlimit_nofile              65535;
 error_log                         /var/log/nginx/error.log;
 include                           /etc/nginx/modules-enabled/*.conf;
-include                           /etc/nginx/conf.d/*.conf;
 
 events {
     multi_accept                  on;
@@ -589,7 +588,7 @@ events {
 
 http {
     log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
-                      '$\status \$body_bytes_sent "\$http_referer" '
+                      '\$status \$body_bytes_sent "\$http_referer" '
                       '"\$http_user_agent" "\$http_x_forwarded_for"';
 
 #   access_log                    /var/log/nginx/access.log  main;
@@ -628,6 +627,8 @@ http {
     resolver_timeout              2s;
 
     gzip                          on;
+    
+    include                       /etc/nginx/conf.d/*.conf;
 }
 
 stream {
