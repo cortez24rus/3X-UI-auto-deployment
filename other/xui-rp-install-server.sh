78e4b50ad583c82e97dbea0833284ea4094f8306
cortez24rus <89983388+cortez24rus@users.noreply.github.com>
2024-11-10 03:25:37 +0700

    Update xui-rp-install-server.sh

--- other/xui-rp-install-server.sh
+++ other/xui-rp-install-server.sh
@@ -768,12 +768,15 @@ server {
 #        auth_basic "Restricted Content";
 #        auth_basic_user_file /etc/nginx/.htpasswd;
 #    }
-    location /metrics {
-        proxy_pass http://127.0.0.1:9100;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
+     location /secret_node_metrics {
+        proxy_pass http://127.0.0.1:9100/metrics;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+
+        auth_basic "Restricted Content";
+        auth_basic_user_file /etc/nginx/.htpasswd;
     }
     # X-ui Admin panel
     location /${webBasePath} {
