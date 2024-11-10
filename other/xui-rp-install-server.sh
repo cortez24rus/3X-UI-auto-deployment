e70f5b015a83f3e72d825e0ba21e64660aac3cc0
cortez24rus <89983388+cortez24rus@users.noreply.github.com>
2024-11-10 13:18:57 +0700

    Update xui-rp-install-server.sh

--- other/xui-rp-install-server.sh
+++ other/xui-rp-install-server.sh
@@ -778,6 +778,16 @@ server {
         auth_basic "Restricted Content";
         auth_basic_user_file /etc/nginx/.htpasswd;
     }
+    location ~* /(sub|dashboard|api|docs|redoc|openapi.json|statics) {
+        proxy_redirect off;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_pass https://127.0.0.1:8000;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    }
     # X-ui Admin panel
     location /${webBasePath} {
         proxy_redirect off;
