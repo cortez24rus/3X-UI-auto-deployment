4446fe9e06815328ffde1d1c7ca288f4f9729372
cortez24rus <89983388+cortez24rus@users.noreply.github.com>
2024-11-12 00:51:54 +0700

    Update xui-rp-install-server.sh

--- xui-rp-install-server.sh
+++ xui-rp-install-server.sh
@@ -516,7 +516,7 @@ warp() {
         sleep 3
     done
     chmod +x xui-rp-warp.sh && ./xui-rp-warp.sh
-    rm -rf xui-rp-warp.sh*
+    rm -rf xui-rp-warp.sh
     echo
     msg_tilda "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
     echo
