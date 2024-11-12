e0003f902d6cb321dacf33e21b377380fea51faa
cortez24rus <89983388+cortez24rus@users.noreply.github.com>
2024-11-12 09:38:31 +0700

    Update xui-rp-warp.sh

--- warp/xui-rp-warp.sh
+++ warp/xui-rp-warp.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
 # Установка ключа и репозитория Cloudflare WARP
-curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(grep "VERSION_CODENAME=" /etc/os-release | cut -d "=" -f 2) main" | tee /etc/apt/sources.list.d/cloudflare-client.list
-apt-get update && apt-get install cloudflare-warp -y
-
-sleep 3
-# Принятие условий использования и регистрация
-warp-cli --accept-tos registration new
-warp-cli --accept-tos mode proxy
-warp-cli --accept-tos proxy port 40000
-warp-cli --accept-tos connect
+#curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+#echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(grep "VERSION_CODENAME=" /etc/os-release | cut -d "=" -f 2) main" | tee /etc/apt/sources.list.d/cloudflare-client.list
+#apt-get update && apt-get install cloudflare-warp -y
 
 # Установка конкретной версии Cloudflare WARP
 wget https://pkg.cloudflareclient.com/pool/$(grep "VERSION_CODENAME=" /etc/os-release | cut -d "=" -f 2)/main/c/cloudflare-warp/cloudflare-warp_2024.6.497-1_amd64.deb > /dev/null 2>&1
@@ -31,8 +24,16 @@ systemctl daemon-reload
 systemctl restart warp-svc.service
 sleep 5  # Ожидание запуска демона
 
+# Принятие условий использования и регистрация
+warp-cli --accept-tos registration new
+warp-cli --accept-tos mode proxy
+warp-cli --accept-tos proxy port 40000
+warp-cli --accept-tos connect
+
 # Проверка подключения через прокси
 curl -x socks5h://localhost:40000 https://2ip.io
 
 # Статистика туннеля (опционально)
 warp-cli tunnel stats
+echo "ПОЛИТИКИ НУЖНО ПРИНЯТЬ САМОМУ, БЕЗ ЭТОГО ВАРП НЕ ЗАРАБОТАЕТ!!!"
+echo "Введите warp-cli status, если не было или пропустили сообщение о принятии." 
