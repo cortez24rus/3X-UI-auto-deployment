51d6f99f8fd5c810f63db26dc4f0e9915b746575
cortez24rus <89983388+cortez24rus@users.noreply.github.com>
2024-11-12 08:58:54 +0700

    Update xui-rp-warp.sh

--- warp/xui-rp-warp.sh
+++ warp/xui-rp-warp.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
+# Установка ключа и репозитория Cloudflare WARP
 curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(grep "VERSION_CODENAME=" /etc/os-release | cut -d "=" -f 2) main" | tee /etc/apt/sources.list.d/cloudflare-client.list
 apt-get update && apt-get install cloudflare-warp -y
+
+# Установка конкретной версии Cloudflare WARP
 wget https://pkg.cloudflareclient.com/pool/$(grep "VERSION_CODENAME=" /etc/os-release | cut -d "=" -f 2)/main/c/cloudflare-warp/cloudflare-warp_2024.6.497-1_amd64.deb > /dev/null 2>&1
 dpkg -i cloudflare-warp_2024.6.497-1_amd64.deb
 
-warp-cli status
-echo -e "yes" | warp-cli --accept-tos registration new
-#warp-cli --accept-tos mode proxy
-#warp-cli --accept-tos proxy port 40000
-#warp-cli --accept-tos connect
-#yes | warp-cli registration new
-#warp-cli --accept-tos registration new
-warp-cli mode proxy
-warp-cli proxy port 40000
-warp-cli connect
+# Принятие условий использования и регистрация
+warp-cli --accept-tos registration new
+warp-cli --accept-tos mode proxy
+warp-cli --accept-tos proxy port 40000
+warp-cli --accept-tos connect
 
+# Удаление временных файлов
 rm -rf cloudflare-warp_*
 mkdir -p /etc/systemd/system/warp-svc.service.d
 
+# Настройка уровня логирования
 cat > /etc/systemd/system/warp-svc.service.d/override.conf <<EOF
 [Service]
 LogLevelMax=3
 EOF
 
+# Перезагрузка демона и ожидание его запуска
 systemctl daemon-reload
 systemctl restart warp-svc.service
+sleep 5  # Ожидание запуска демона
 
-sleep 5
+# Проверка подключения через прокси
 curl -x socks5h://localhost:40000 https://2ip.io
+
+# Статистика туннеля (опционально)
 warp-cli tunnel stats
