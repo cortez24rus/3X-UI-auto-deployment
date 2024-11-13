e4d3da44223ef59c5a80d9c512347e6799d06328
cortez24rus <89983388+cortez24rus@users.noreply.github.com>
2024-11-13 12:54:22 +0700

    Update xui-rp-install-server.sh

--- xui-rp-install-server.sh
+++ xui-rp-install-server.sh
@@ -718,7 +718,7 @@ panel_installation() {
 
     x-ui stop
     
-    mv /etc/x-ui/x-ui.db  /etc/x-ui/x-ui.db.backup
+    mv /etc/x-ui/x-ui.db /etc/x-ui/x-ui.db.backup
     mv x-ui.db /etc/x-ui/
     x-ui start
     echo
