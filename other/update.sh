b7213ef865207b7cf770bea58737452c4812802f
cortez <89983388+cortez24rus@users.noreply.github.com>
2024-11-29 19:33:51 +0700

    Update update.sh

--- other/update.sh
+++ other/update.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
 history_file="/var/log/apt/history.log"
-# Извлекаем последнюю дату обновления
-last_upgrade_date=$(grep "Start-Date:" $history_file | tail -n 1 | awk '{print $2, $3}')
-last_upgrade_time=$(grep "Start-Date:" $history_file | tail -n 1 | awk '{print $4}')
-echo $last_upgrade_date
-echo $last_upgrade_time
 
-# Преобразуем дату и время в timestamp
-last_upgrade_timestamp=$(date -d "${last_upgrade_date} ${last_upgrade_time}" +%s 2>/dev/null)
-if [ -z "$last_upgrade_timestamp" ]; then
+# Извлекаем последнюю дату обновления
+last_upgrade_date=$(grep "Start-Date:" "$history_file" | awk '{print $2}' | sort -r | head -n 1)
+if [ -z "$last_upgrade_date" ]; then
     echo "Не удалось извлечь дату последнего обновления."
     exit 1
 fi
 
-echo $last_upgrade_timestamp
-
-# Текущая дата в timestamp
-current_timestamp=$(date +%s)
+# Текущая дата
+current_date=$(date +%Y-%m-%d)
 
 # Разница в днях
-days_diff=$(( (current_timestamp - last_upgrade_timestamp) / 86400 ))
+days_diff=$(( ( $(date -d "$current_date" +%s) - $(date -d "$last_upgrade_date" +%s) ) / 86400 ))
 
+echo $last_upgrade_date
 echo $days_diff
 
+# Проверяем разницу
 if [ $days_diff -le 3 ]; then
     echo "Система обновлена."
 else
