1b3a90ae54099726e17a4855a27a24501fe0e566
cortez <89983388+cortez24rus@users.noreply.github.com>
2024-11-29 19:28:29 +0700

    Update update.sh

--- other/update.sh
+++ other/update.sh
@@ -4,6 +4,8 @@ history_file="/var/log/apt/history.log"
 # Извлекаем последнюю дату обновления
 last_upgrade_date=$(grep "Start-Date:" $history_file | tail -n 1 | awk '{print $2, $3}')
 last_upgrade_time=$(grep "Start-Date:" $history_file | tail -n 1 | awk '{print $4}')
+echo $last_upgrade_date
+echo $last_upgrade_time
 
 # Преобразуем дату и время в timestamp
 last_upgrade_timestamp=$(date -d "${last_upgrade_date} ${last_upgrade_time}" +%s 2>/dev/null)
@@ -12,11 +14,15 @@ if [ -z "$last_upgrade_timestamp" ]; then
     exit 1
 fi
 
+echo $last_upgrade_timestamp
+
 # Текущая дата в timestamp
 current_timestamp=$(date +%s)
 
 # Разница в днях
 days_diff=$(( (current_timestamp - last_upgrade_timestamp) / 86400 ))
+
+echo $days_diff
 
 if [ $days_diff -le 3 ]; then
     echo "Система обновлена."
