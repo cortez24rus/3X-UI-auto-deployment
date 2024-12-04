95fca038922dd7c0fd59f562b445afbf1d95a1ab
cortez <89983388+cortez24rus@users.noreply.github.com>
2024-12-05 00:29:32 +0700

    nginx fix #4

--- other/123.sh
+++ other/123.sh
@@ -20,20 +20,38 @@ cd nginx-$NGINX_VERSION
 
 git clone https://github.com/leev/ngx_http_geoip2_module.git
 
-./configure --prefix=/usr/share/nginx --conf-path=/etc/nginx/nginx.conf \
-    --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log \
-    --lock-path=/var/lock/nginx.lock --pid-path=/run/nginx.pid --modules-path=/usr/lib/nginx/modules \
-    --http-client-body-temp-path=/var/lib/nginx/body --http-fastcgi-temp-path=/var/lib/nginx/fastcgi \
-    --http-proxy-temp-path=/var/lib/nginx/proxy --http-scgi-temp-path=/var/lib/nginx/scgi \
-    --http-uwsgi-temp-path=/var/lib/nginx/uwsgi --with-debug --with-compat --with-pcre-jit \
-    --with-http_ssl_module --with-http_stub_status_module --with-http_realip_module \
-    --with-http_auth_request_module --with-http_v2_module --with-http_dav_module \
-    --with-http_slice_module --with-threads --with-http_addition_module --with-http_gunzip_module \
-    --with-http_gzip_static_module --with-http_image_filter_module=dynamic --with-http_sub_module \
-    --with-http_xslt_module=dynamic --with-stream=dynamic --with-stream_ssl_module \
-    --with-mail=dynamic --with-mail_ssl_module --with-http_mp4_module \
-    --add-module=./ngx_http_geoip2_module
-    
+./configure \
+    --prefix=/usr/share/nginx \
+    --conf-path=/etc/nginx/nginx.conf \
+    --http-log-path=/var/log/nginx/access.log \
+    --error-log-path=/var/log/nginx/error.log \
+    --lock-path=/var/lock/nginx.lock \
+    --pid-path=/run/nginx.pid \
+    --modules-path=/usr/lib/nginx/modules \
+    --http-client-body-temp-path=/var/lib/nginx/body \
+    --http-fastcgi-temp-path=/var/lib/nginx/fastcgi \
+    --http-proxy-temp-path=/var/lib/nginx/proxy \
+    --http-scgi-temp-path=/var/lib/nginx/scgi \
+    --http-uwsgi-temp-path=/var/lib/nginx/uwsgi \
+    --with-compat \
+    --with-pcre-jit \
+    --with-http_ssl_module \
+    --with-http_stub_status_module \
+    --with-http_realip_module \
+    --with-http_v2_module \
+    --with-http_dav_module \
+    --with-http_slice_module \
+    --with-threads \
+    --with-http_addition_module \
+    --with-http_gunzip_module \
+    --with-http_gzip_static_module \
+    --with-http_image_filter_module=dynamic \
+    --with-http_sub_module \
+    --with-http_xslt_module=dynamic \
+    --with-stream \
+    --with-stream_ssl_module \
+    --add-dynamic-module=./ngx_http_geoip2_module
+
 make
 make install
 
