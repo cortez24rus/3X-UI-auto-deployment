e1bf44d4d1215791c3acf1f7311e47af4f6389c2
cortez <89983388+cortez24rus@users.noreply.github.com>
2024-12-05 20:42:17 +0700

    Update make_nginx.sh

--- other/make_nginx.sh
+++ other/make_nginx.sh
@@ -63,16 +63,16 @@ cat > /etc/systemd/system/nginx.service <<EOF
 Description=The NGINX HTTP and reverse proxy server
 After=syslog.target network-online.target remote-fs.target nss-lookup.target
 Wants=network-online.target
-        
+
 [Service]
 Type=forking
 PIDFile=/run/nginx.pid
 ExecStartPre=/usr/sbin/nginx -t
-ExecStart=/usr/sbin/nginx
-ExecReload=/usr/sbin/nginx -s reload
-ExecStop=/bin/kill -s QUIT $MAINPID
-PrivateTmp=true
-        
+ExecStart=/usr/sbin/nginx -c /etc/nginx/nginx.conf
+ExecReload=/bin/sh -c "/bin/kill -s HUP $(/bin/cat /run/nginx.pid)"
+ExecStop=/bin/sh -c "/bin/kill -s TERM $(/bin/cat /run/nginx.pid)"
+#PrivateTmp=true
+
 [Install]
 WantedBy=multi-user.target
 EOF
