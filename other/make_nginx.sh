b738745cf1227e0d79de96e660c19f2bd80515ac
cortez <89983388+cortez24rus@users.noreply.github.com>
2024-12-05 19:52:12 +0700

    Update make_nginx.sh

--- other/make_nginx.sh
+++ other/make_nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update && apt install -y \
+sudo apt update && apt install -y \
     build-essential \
     libpcre++-dev \
     libssl-dev \
@@ -17,7 +17,6 @@ NGINX_VERSION="1.27.3"
 wget https://nginx.org/download/nginx-$NGINX_VERSION.tar.gz
 tar -xvf nginx-$NGINX_VERSION.tar.gz
 cd nginx-$NGINX_VERSION
-
 git clone https://github.com/leev/ngx_http_geoip2_module.git
 
 ./configure \
@@ -26,46 +25,38 @@ git clone https://github.com/leev/ngx_http_geoip2_module.git
     --conf-path=/etc/nginx/nginx.conf \
     --http-log-path=/var/log/nginx/access.log \
     --error-log-path=/var/log/nginx/error.log \
-    --lock-path=/var/lock/nginx.lock \
-    --pid-path=/var/run/nginx.pid \
+    --lock-path=/run/nginx.lock \
+    --pid-path=/run/nginx.pid \
     --modules-path=/usr/lib/nginx/modules \
     --http-client-body-temp-path=/var/lib/nginx/body \
     --http-fastcgi-temp-path=/var/lib/nginx/fastcgi \
     --http-proxy-temp-path=/var/lib/nginx/proxy \
     --http-scgi-temp-path=/var/lib/nginx/scgi \
     --http-uwsgi-temp-path=/var/lib/nginx/uwsgi \
-    --with-compat \
-    --with-pcre-jit \
     --with-http_ssl_module \
     --with-http_stub_status_module \
     --with-http_realip_module \
     --with-http_v2_module \
-    --with-http_dav_module \
-    --with-http_slice_module \
-    --with-threads \
-    --with-http_addition_module \
-    --with-http_gunzip_module \
-    --with-http_gzip_static_module \
-    --with-http_image_filter_module=dynamic \
-    --with-http_sub_module \
-    --with-http_xslt_module=dynamic \
     --with-stream \
     --with-stream_ssl_module \
+    --with-http_gunzip_module \
     --add-dynamic-module=./ngx_http_geoip2_module
-
-#--with-http_ssl_module \
-#--with-http_stub_status_module \
-#--with-http_realip_module \
-#--with-http_v2_module \
-#--with-stream \
-#--with-stream_ssl_module \
-#--add-dynamic-module=./ngx_http_geoip2_module
-
+#    --with-compat \
+#    --with-pcre-jit \
+#    --with-http_dav_module \
+#    --with-http_slice_module \
+#    --with-threads \
+#    --with-http_addition_module \
+#    --with-http_image_filter_module=dynamic \
+#    --with-http_sub_module \
+#    --with-http_xslt_module=dynamic \
 
 make
 make install
 
-sudo /usr/sbin/nginx
+mkdir -p /var/lib/nginx/body
+chown -R www-data:www-data /var/lib/nginx
+chmod -R 700 /var/lib/nginx
 
 cat > /etc/systemd/system/nginx.service <<EOF
 [Unit]
@@ -75,7 +66,7 @@ Wants=network-online.target
         
 [Service]
 Type=forking
-PIDFile=/var/run/nginx.pid
+PIDFile=/run/nginx.pid
 ExecStartPre=/usr/sbin/nginx -t
 ExecStart=/usr/sbin/nginx
 ExecReload=/usr/sbin/nginx -s reload
@@ -86,12 +77,12 @@ PrivateTmp=true
 WantedBy=multi-user.target
 EOF
 
-systemctl daemon-reload
-systemctl start nginx
-systemctl enable nginx
+sudo systemctl daemon-reload
+sudo systemctl start nginx
+sudo systemctl enable nginx
 
 cd ..
-rm -rf nginx-$NGINX_VERSION.tar.gz nginx-$NGINX_VERSION ngx_http_geoip2_module
+sudo rm -rf nginx-$NGINX_VERSION.tar.gz nginx-$NGINX_VERSION ngx_http_geoip2_module
 
 systemctl status nginx
 nginx -v
