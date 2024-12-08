0f6a666d6073ca550211749dbe30c7c5c27912ed
cortez <89983388+cortez24rus@users.noreply.github.com>
2024-12-08 15:06:46 +0700

    make fix#1

--- other/make_nginx.sh
+++ other/make_nginx.sh
@@ -21,28 +21,49 @@ cd nginx-$NGINX_VERSION
 git clone https://github.com/leev/ngx_http_geoip2_module.git
 
 ./configure \
-    --sbin-path=/usr/sbin/nginx \
     --prefix=/etc/nginx \
-    --conf-path=/etc/nginx/nginx.conf \
-    --http-log-path=/var/log/nginx/access.log \
-    --error-log-path=/var/log/nginx/error.log \
-    --lock-path=/run/nginx.lock \
-    --pid-path=/run/nginx.pid \
+    --sbin-path=/usr/sbin/nginx \
     --modules-path=/usr/lib/nginx/modules \
-    --http-client-body-temp-path=/var/lib/nginx/body \
-    --http-fastcgi-temp-path=/var/lib/nginx/fastcgi \
-    --http-proxy-temp-path=/var/lib/nginx/proxy \
-    --http-scgi-temp-path=/var/lib/nginx/scgi \
-    --http-uwsgi-temp-path=/var/lib/nginx/uwsgi \
+    --conf-path=/etc/nginx/nginx.conf \
+    --error-log-path=/var/log/nginx/error.log \
+    --http-log-path=/var/log/nginx/access.log \
+    --pid-path=/run/nginx.pid \
+    --lock-path=/run/nginx.lock \
+    --http-client-body-temp-path=/var/cache/nginx/client_temp \
+    --http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+    --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+    --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
+    --http-scgi-temp-path=/var/cache/nginx/scgi_temp \
+    --user=nginx \
+    --group=nginx \
+    --with-compat \
+    --with-file-aio \
+    --with-threads \
+    --with-http_addition_module \
+    --with-http_auth_request_module \
+    --with-http_dav_module \
+    --with-http_flv_module \
+    --with-http_gunzip_module \
+    --with-http_gzip_static_module \
+    --with-http_mp4_module \
+    --with-http_random_index_module \
+    --with-http_realip_module \
+    --with-http_secure_link_module \
+    --with-http_slice_module \
     --with-http_ssl_module \
     --with-http_stub_status_module \
-    --with-http_realip_module \
+    --with-http_sub_module \
     --with-http_v2_module \
+    --with-http_v3_module \
+    --with-mail \
+    --with-mail_ssl_module \
     --with-stream \
+    --with-stream_realip_module \
     --with-stream_ssl_module \
     --with-stream_ssl_preread_module \
-    --with-http_gunzip_module \
-    --add-dynamic-module=./ngx_http_geoip2_module
+    --add-dynamic-module=./ngx_http_geoip2_module \
+    --with-cc-opt="-g -O2 -ffile-prefix-map=$(pwd)/${NGINX_VERSION}=${PWD}/${NGINX_VERSION} -fstack-protector-strong -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -fPIC" \
+    --with-ld-opt="-Wl,-z,relro -Wl,-z,now -Wl,--as-needed -pie"
 
 make
 make install
