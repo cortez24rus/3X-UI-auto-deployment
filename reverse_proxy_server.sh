fa527ef6e6334d4669d830eb65bbe76a70427b8a
cortez <89983388+cortez24rus@users.noreply.github.com>
2024-12-19 14:43:46 +0700

    test 5

--- reverse_proxy_server.sh
+++ reverse_proxy_server.sh
@@ -260,7 +260,7 @@ show_help() {
   echo "                                 Интеграция Telegram бота"
   echo "  -g, --generate <true|false>    Generate a random string for configuration       (default: ${defaults[generate]})"
   echo "                                 Генерация случайных путей для конфигурации"
-  echo "  -x, --skip-check <true|false>  Disable the check functionality                  (default: ${defaults[skip_check]})"
+  echo "  -x, --skip-check <true|false>  Disable the check functionality                  (default: ${defaults[skip-check]})"
   echo "                                 Отключение проверки"
   echo "  -h, --help                     Display this help message                        "
   echo "                                 Показать это сообщение помощи"
@@ -296,7 +296,7 @@ read_defaults_from_file() {
     defaults[ssh]=true
     defaults[tgbot]=false
     defaults[generate]=true
-    defaults[skip_check]=false
+    defaults[skip-check]=false
   fi
 }
 
@@ -321,7 +321,7 @@ defaults[firewall]=false
 defaults[ssh]=false
 defaults[tgbot]=false
 defaults[generate]=true
-defaults[skip_check]=false
+defaults[skip-check]=false
 EOF
 }
 
@@ -456,9 +456,9 @@ parse_args() {
         shift 2
         ;;
       -x|--skip-check)
-        args[skip_check]="$2"
-        normalize_case skip_check
-        validate_true_false skip_check "$2" || return 1
+        args[skip-check]="$2"
+        normalize_case skip-check
+        validate_true_false skip-check "$2" || return 1
         shift 2
         ;;
       -h|--help)
@@ -738,7 +738,7 @@ check_cf_token() {
     fi
 
     echo "1"
-    [[ ${args[skip_check]} == "false" ]] && check_domain_ip
+    [[ ${args[skip-check]} == "false" ]] && check_domain_ip
     echo "2"
 
     while [[ -z $EMAIL ]]; do
@@ -749,7 +749,7 @@ check_cf_token() {
     while [[ -z $CFTOKEN ]]; do
       reading " $(text 16) " CFTOKEN
     done
-    [[ ${args[skip_check]} == "false" ]] && get_test_response
+    [[ ${args[skip-check]} == "false" ]] && get_test_response
     info " $(text 17) "
   done
 }
@@ -2007,7 +2007,7 @@ main() {
   log_entry
   read_defaults_from_file
   parse_args "$@" || show_help
-  [[ ${args[skip_check]} == "false" ]] && check_root
+  [[ ${args[skip-check]} == "false" ]] && check_root
   check_operating_system
   select_language
   if [ -f ${defaults_file} ]; then
@@ -2016,7 +2016,7 @@ main() {
   sleep 2
   clear
   banner_1
-  [[ ${args[skip_check]} == "false" ]] && start_installation
+  [[ ${args[skip-check]} == "false" ]] && start_installation
   data_entry
   [[ ${args[utils]} == "true" ]] && installation_of_utilities
   [[ ${args[dns]} == "true" ]] && dns_encryption
